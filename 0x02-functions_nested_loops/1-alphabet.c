280ea260933d661ea75d2cc6ca5936654cc5d81b
omarovici <omarkhalidalx2004@gmail.com>
2023-07-17 21:25:59 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - main
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
