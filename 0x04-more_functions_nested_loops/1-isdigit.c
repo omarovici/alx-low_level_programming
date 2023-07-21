27732518ca4d5f6e9e75beb565661adb74e193ee
omarovici <omarkhalidalx2004@gmail.com>
2023-07-20 17:08:12 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - check
+ * @c: int
+ * Return: 1
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+
+#include "main.h"
+/**
+ * mul - check
+ * @a: int
+ * @b: int
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
